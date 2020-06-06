ea50cd60ec94e0db0b347112ad8e5499596649bc
Arslan Sajid <arslansajid951@gmail.com>
2020-06-06 17:43:10 +0500

    updated footer component

--- src/components/Footers/DemoFooter.js
+++ src/components/Footers/DemoFooter.js
@@ -1,53 +1,35 @@
 import React from "react";
 
 // reactstrap components
-import { Row, Container } from "reactstrap";
+import { Col, Row, Container } from "reactstrap";
 
 function DemoFooter() {
   return (
-    <footer className="footer lightgrey-bg">
-      <Container>
-        <Row className="justify-content-center">
-          <nav className="footer-nav">
-            <ul>
-              <li>
-                <a
-                  href="https://twitter.com/home"
-                  target="_blank"
-                >
-                  {/* Facebook */}
-                  <i className="fa fa-2x fa-twitter" />
-                  <p className="d-none">Twitter</p>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://www.facebook.com/"
-                  target="_blank"
-                >
-                  {/* Instagram */}
-                  <i className="fa fa-2x fa-facebook-square" />
-                  <p className="d-none">Facebook</p>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://instagram.com"
-                  target="_blank"
-                >
-                  {/* Licenses */}
-                  <i className="fa fa-2x fa-instagram" />
-                  <p className="d-none">Instagram</p>
-                </a>
-              </li>
-            </ul>
-          </nav>
-          {/* <div className="credits ml-auto">
-            <span className="copyright">
-              © {new Date().getFullYear()}, made with{" "}
-              <i className="fa fa-heart heart" /> by Meraj Housing
-            </span>
-          </div> */}
+    <footer className="footer">
+      <h3 className="title text-center">CONTACT US</h3>
+      <Container fluid>
+        <Row className="">
+          <Col lg={4} md={6} sm={12}>
+            <div className=" footer-card border-right">
+              <img className="footer-icon" src={require("assets/img/footer/call.png")} />
+              <div>+92 300 8512727</div>
+            </div>
+          </Col>
+          <Col lg={4} md={6} sm={12}>
+            <div className=" footer-card border-right">
+            <img className="footer-icon" src={require("assets/img/footer/map.png")} />
+            <div>
+              3KM Main Daska Road towards Sialkot, <br />
+              Near Sialkot-Lahore Motorway Interchange point, <br />
+              Tehsil Daska, District Sialkot.</div>
+            </div>
+          </Col>
+          <Col lg={4} md={6} sm={12}>
+            <div className=" footer-card">
+            <img className="footer-icon" src={require("assets/img/footer/msg.png")} />
+              <div>khurram@virkgroup.com</div>
+            </div>
+          </Col>
         </Row>
       </Container>
     </footer>
